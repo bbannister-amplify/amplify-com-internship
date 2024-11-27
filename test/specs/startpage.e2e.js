8be0167eb5f990d597b1db89d3e578029f15492b
Bob Bannister <bbannister@amplify.com>
2024-11-27 17:35:45 -0500

    tweaks to startpage test

--- test/specs/startpage.e2e.js
+++ test/specs/startpage.e2e.js
@@ -5,11 +5,7 @@ describe('Amplify.com', () => {
     it('should go to the landing page', async () => {
         await StartPage.open()
 
-       /* await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveText(
-            expect.stringContaining('You logged into a secure area!')) */
-        await expect($('.icon icon--logo').toBeVisible())
+        await expect($('.icon.icon--logo').toBeVisible())
         await browser.pause(5000);
     })
 })
