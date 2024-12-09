da567509497cd8c261daf770559ff352795d837c
Desmond Bishop <k2db2008@gmail.com>
2024-12-09 17:36:29 -0500

    new resources page test

---
+++ test/specs/resourcespage.e2e.js
@@ -0,0 +1,20 @@
+import { expect } from '@wdio/globals'
+import ResourcesPage from '../pageobjects/resources.page.js'
+
+describe('Amplify.com', () => {
+    it('should go to the landing page', async () => {
+        await ResourcesPage.open()
+
+        await expect($('.icon.icon--logo').toBeVisible())
+        const buttonOne = await $('#m-programs').getText()
+        await expect(buttonOne).toContain('Programs')
+        const buttonTwo = await $('#m-services').getText()
+        await expect(buttonTwo).toContain('Services')
+        const buttonThree = await $('#m-resources').getText()
+        await expect(buttonThree).toContain('Resources')
+        const buttonFour = await $('#m-community').getText()
+        await expect(buttonFour).toContain('Community')
+        const buttonFive = await $('#m-support').getText()
+        await expect(buttonFive).toContain('Support')
+    })
+})
