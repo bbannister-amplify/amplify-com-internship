f7bc856a46147d82aad5a579806d60df85af95fa
Bob Bannister <bbannister@amplify.com>
2024-12-12 11:46:55 -0500

    moving logo check to shared page object

--- test/specs/knowledgeBuilding.e2e.js
+++ test/specs/knowledgeBuilding.e2e.js
@@ -1,11 +1,12 @@
 import { expect } from '@wdio/globals'
 import KnowledgePage from '../pageobjects/knowledge.page.js'
+import SharedPage from '../pageobjects/shared.page.js'
 
 describe('Amplify.com', () => {
     it('should go to the knowledge building page', async () => {
         await KnowledgePage.open();
 
-        await expect($('.icon.icon--logo').toBeVisible());
+        await SharedPage.checkLogo()
 
         await expect($('.m-salesButton__main').toBeVisible());
         const expertButton = await $('.m-salesButton__main').getText();
--- test/specs/startpage.e2e.js
+++ test/specs/startpage.e2e.js
@@ -1,12 +1,11 @@
 import { expect } from '@wdio/globals'
 import StartPage from '../pageobjects/start.page.js'
+import SharedPage from '../pageobjects/shared.page.js'
 
 describe('Amplify.com', () => {
-    it('should go to the landing page', async () => {
+    it('should go to the landing page and check the for the Amplify logo', async () => {
         await StartPage.open()
-
-        await expect($('.icon.icon--logo').toBeVisible())
-
+        await SharedPage.checkLogo()
     })
 })
 
