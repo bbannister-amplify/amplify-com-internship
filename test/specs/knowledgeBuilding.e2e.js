d65535598dd14aa980b3ddab572bbcf13554577f
Bob Bannister <bbannister@amplify.com>
2024-12-12 11:48:49 -0500

    updated spec

--- test/specs/knowledgeBuilding.e2e.js
+++ test/specs/knowledgeBuilding.e2e.js
@@ -6,10 +6,6 @@ describe('Amplify.com', () => {
         await KnowledgePage.open();
 
         await expect($('.icon.icon--logo').toBeVisible());
-
-        await expect($('.m-salesButton__main').toBeVisible());
-        const expertButton = await $('.m-salesButton__main').getText();
-        await expect(expertButton).toContain('Connect with a product expert');
     })
 })
 
